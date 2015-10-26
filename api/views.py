d2a0e2427fda6b1566024102266982a6e08cbe56
Horácio Dias <horacio.neto@supportweb.com.br>
2015-10-26 10:09:16 -0200

    Implementando filtro na mão

--- api/views.py
+++ api/views.py
@@ -1,8 +1,18 @@
+import django_filters
 from rest_framework_mongoengine.viewsets import ModelViewSet
+from rest_framework import filters
 from .mixins import DefaultMixin, LoginMixin
 from .serializers import *
 from .models import *
 
 class UsuarioViewSet(DefaultMixin, ModelViewSet):
     serializer_class = UsuarioSerializer
-    queryset = Usuario.objects.all()
+    #queryset = Usuario.objects.all()
+
+    def get_queryset(self):
+        queryset = Usuario.objects.all()
+        if self.request.GET.get('email'):
+            queryset = queryset.filter(email=self.request.GET['email'])
+        if self.request.GET.get('nome'):
+            queryset = queryset.filter(nome=self.request.GET['nome'])
+        return queryset
